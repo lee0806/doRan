5a31a06a35e7bb42ebaa832dc5971b6d76db35a9
sehyun lee <mh080411@kyonggi.ac.kr>
2025-06-23 16:59:12 +0900

    feat: React, Vite, Tailwind CSS로 daSom 프로젝트 초기화하기
    - React와 Tailwind CSS를 통합하기 위해 Vite 구성 파일을 만들었습니다.

---
+++ daSom/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold justify-center origin-center flex mt-36">테스트 중입니다.</div>
+    </div>
+  );
+}
+
+export default App;
